55d2b12f9fdaa936936846e5748057adcacd4866
nm5005 <nm5005@snu.ac.kr>
2017-06-16 14:58:13 +0000

    ta_index change and student_index change

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
  User.create name: '조인성', email: 'ta@ta.com', password: '12341234', is_ta: true
  User.create name: '하정우', email: 'student93@student.com', password: '12341234', is_ta: false
- 
+ User.create name: '구자운', email: 'a@a.com', password: '12341234', is_ta: false, seat_number:2
+ User.create name: '김은수', email: 'b@b.com', password: '12341234', is_ta: false, seat_number:3, emergency_help:true
+ User.create name: '이윤성', email: 'c@c.com', password: '12341234', is_ta: false, seat_number:2, emergency_help:true
+ User.create name: '정현우', email: 'd@d.com', password: '12341234', is_ta: false 
+ User.create name: '조정원', email: 'e@e.com', password: '12341234', is_ta: false
