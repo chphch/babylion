f4f17b3eb180a8f3a8479cb074b9a2d85c817d09
HyunWoo <chaosphch@gmail.com>
2017-06-24 15:17:01 +0900

    ta_index

--- app/controllers/ta_controller.rb
+++ app/controllers/ta_controller.rb
@@ -1,7 +1,18 @@
 class TaController < ApplicationController
     
   def ta_index
-      @questions = Question.all
+    @last_id = Question.last.id
+    if (@last_id == nil)
+      @last_id = 0
+    end
+    @questions = Question.all
+  end
+
+  def update_ta_index_questions
+    last_id = params[:last_id]
+    @questions = Question.where("id > #{last_id}")
+#    @questions = Question.all
+    render json: @questions
   end
 
    
